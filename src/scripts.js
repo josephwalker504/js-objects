a4c2a270ccd4ed9fefe354b6a721d7fec3b1f49e
joseph walker <josephwalker504@gmail.com>
2019-10-09 15:36:31 -0500

    Next set of code for that exercise

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,6 @@
 console.log("Maybe I'm getting it");
+console.log(wardrobe.height)
+console.log(wardrobe.manufacture)
+console.log(wardrobe.contents)
+console.log(wardrobe.depth)
+console.log(wardrobe.width)
