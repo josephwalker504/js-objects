94db41ae6bc45c1b2d6bd5c3953da9d2fcaf79f3
joseph walker <josephwalker504@gmail.com>
2019-10-09 22:03:09 -0500

    Lighting exercise three practice exercise code

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,35 @@
 console.log("Maybe I'm getting it");
+
+const empireStateBuilding = {
+    stories: 103,
+    height: 1453,
+    address: "350 Fifth Avenue, Manhattan, New York 10118",
+    squareFeet: 2768591,
+    constructionDate: 1931,
+    cost: 40948900,
+    owner: "Empire State Realty Trust",
+    eastWestLength: 424,
+    northSouthLength: 187,
+    architect: "Shreve, Lamb & Harmon"
+}
+console.log(empireStateBuilding.stories);
+console.log(empireStateBuilding.height);
+console.log(empireStateBuilding.squareFeet);
+console.log(empireStateBuilding.eastWestLength);
+console.log(empireStateBuilding.northSouthLength);
+
+const empireAddress = "address";
+const constructionDateYear = "constructionDate";
+const costCost ="cost";
+const ownerOwner = "owner";
+const architectArchitect = "architect";
+const empireStateBuildingAddress = empireStateBuilding[empireAddress]
+console.log(empireStateBuildingAddress);
+const empireStateBuildingConstruction = empireStateBuilding[constructionDateYear]
+console.log(empireStateBuildingConstruction)
+const empireStateBuildingCost = empireStateBuilding[costCost]
+console.log(empireStateBuildingCost)
+const empireStateBuildingOwner = empireStateBuilding[ownerOwner]
+console.log(empireStateBuildingOwner)
+const empireStateBuildingArchitect = empireStateBuilding[architectArchitect]
+console.log(empireStateBuildingArchitect)
